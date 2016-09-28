3479e785c7822230bee97dab7ef07ebef497b85a
brigate <brigate@users.noreply.github.com>
2016-09-28 18:10:57 +0200

    My test commit

---
+++ Klasa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StaticClass
+{
+    public static class Klasa
+    {
+        public static void Ispisi()
+        {
+        Console.WriteLine("Test test");
+        Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StaticClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Klasa.Ispisi();
+            Console.ReadLine();
+        }
+    }
+}
